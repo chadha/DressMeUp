d55ce009ad860b69b24858f6dae08e9cca47ec64
lsliwko <lsliwko@gmail.com>
2013-08-09 17:26:14 +0100

    install receiver

--- DressMeUp/src/com/axisapplications/dressme/receiver/InstallReceiver.java
+++ DressMeUp/src/com/axisapplications/dressme/receiver/InstallReceiver.java
@@ -1,5 +1,8 @@
 package com.axisapplications.dressme.receiver;
 
+import com.axisapplications.dressme.activity.DisplayDescriptionActivity;
+import com.axisapplications.dressme.domain.ItemObject;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -9,14 +12,31 @@ import android.util.Log;
 public class InstallReceiver extends BroadcastReceiver {
 
 	public void onReceive(Context context, Intent intent) {
-		Log.i("INSTALL RECEIVER", "Context: " + context);
+		Log.d("INSTALL RECEIVER", "Context: " + context);
 
 		Bundle extras = intent.getExtras();
 		if (extras != null) {
-			Log.i("INSTALL RECEIVER", "Extras:");
+			Log.d("INSTALL RECEIVER", "Extras:");
 			for (String keys : extras.keySet()) {
-				Log.i("INSTALL RECEIVER", keys + " -> " + extras.get(keys));
+				Log.d("INSTALL RECEIVER", keys + " -> " + extras.get(keys));
 			}
+			
+			final String referrer = extras.getString("referrer");
+			Log.i("REFERRER","Referer is: "+referrer);
+			if (referrer!=null) {
+				String[] referrerData	= referrer.split("|");
+				if (referrerData.length==2) {
+					ItemObject.getCurrentItemObject().retailerItemId	= referrerData[0];
+					ItemObject.getCurrentItemObject().retailerLocationId	= referrerData[1];
+					
+					Intent intent2 = new Intent(context,
+							DisplayDescriptionActivity.class);
+					context.startActivity(intent2);
+				}
+			}
+			
+			
+			
 		} else {
 			Log.i("INSTALL RECEIVER", "Extras are null");
 		}
